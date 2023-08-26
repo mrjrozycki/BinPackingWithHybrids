3cbf864074b1d24bd04d5b1f5960a0913dfd7c80
Jakub Rozycki <dev@jrozycki.me>
2023-08-26 17:04:10 +0200

    Add cleaner function for picking algos

--- src/run.py
+++ src/run.py
@@ -7,24 +7,32 @@ import algos.BC as BC
 import algos.MB_FFD as MB_FFD
 import algos.MB_BFD as MB_BFD
 import algos.MB_WFD as MB_WFD
+import algos.Thr_bin_num as Thr_bin_num
 
-if __name__ == "__main__":
-    if sys.argv[1] == "FFD":
+def pick_algo(name):
+    if name == "FFD":
         algo = FFD.FFD()
-    elif sys.argv[1] == "MB_FFD":
+    elif name == "MB_FFD":
         algo = MB_FFD.MB_FFD(sys.argv[3])
-    elif sys.argv[1] == "BFD":
+    elif name == "BFD":
         algo = BFD.BFD()
-    elif sys.argv[1] == "MB_BFD":
+    elif name == "MB_BFD":
         algo = MB_BFD.MB_BFD(sys.argv[3])
-    elif sys.argv[1] == "WFD":
+    elif name == "WFD":
         algo = WFD.WFD()
-    elif sys.argv[1] == "MB_WFD":
+    elif name == "MB_WFD":
         algo = MB_WFD.MB_WFD(sys.argv[3])
-    elif sys.argv[1] == "BC":
+    elif name == "BC":
         algo = BC.BinCentric()
+    elif name == "THR_1":
+        algo = Thr_bin_num.bin_number(algo1=pick_algo(sys.argv[3]), algo2=pick_algo(sys.argv[4]))
     else:
         raise ValueError("Invalid algorithm name")
+    return algo
+
+
+if __name__ == "__main__":
+    algo = pick_algo(sys.argv[1])
     algo.load_instance(sys.argv[2])
     algo.run()
     print(f"Numbers of bins: {len(algo.bins)}")
