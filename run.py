58bd56dde517d895772161c914331172a148fe96
Jakub Rozycki <dev@jrozycki.me>
2023-08-26 13:30:34 +0200

    Add more information in result

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ import algos.IC_WFD as WFD
 import algos.BC as BC
 import algos.MB_FFD as MB_FFD
 import algos.MB_BFD as MB_BFD
+import algos.MB_WFD as MB_WFD
 
 if __name__ == "__main__":
     if sys.argv[1] == "FFD":
@@ -18,13 +19,14 @@ if __name__ == "__main__":
         algo = MB_BFD.MB_BFD(sys.argv[3])
     elif sys.argv[1] == "WFD":
         algo = WFD.WFD()
+    elif sys.argv[1] == "MB_WFD":
+        algo = MB_WFD.MB_WFD(sys.argv[3])
     elif sys.argv[1] == "BC":
         algo = BC.BinCentric()
     else:
         raise ValueError("Invalid algorithm name")
     algo.load_instance(sys.argv[2])
     algo.run()
-    print(len(algo.bins))
-    # for bin in algo.bins:
-    #     for items in bin.get_items():
-    #         print(bin.id, items, end=" ")
+    print(f"Numbers of bins: {len(algo.bins)}")
+    for bin in algo.bins:
+        print(f"""ID: {bin.id}, Capacity left : {bin.get_capacity()}, Items: {bin.get_items()}, Items sizes: {bin.get_items_sizes()}""", end="\n")
